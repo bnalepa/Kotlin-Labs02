c1e3f3bd129760802369d8ea0a09ebfe072c7987
bnalepa <bnalepa@o2.pl>
2024-04-24 14:20:24 +0200

    Display Task List

--- app/src/main/java/com/example/lab_06/lab06/LabActivity.kt
+++ app/src/main/java/com/example/lab_06/lab06/LabActivity.kt
@@ -4,14 +4,22 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.height
+
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CardDefaults
@@ -28,8 +36,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,7 +51,9 @@ import com.example.lab_06.lab06.ui.theme.Lab_06Theme
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.lab_06.R
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 
 class LabActivity : ComponentActivity() {
@@ -62,14 +76,6 @@ class LabActivity : ComponentActivity() {
 @Composable
 fun ListScreen(navController: NavController) {
     Scaffold(
-        topBar = {
-            AppTopBar(
-                navController = navController,
-                title = "Form",
-                showBackIcon = true,
-                route = "list"
-            )
-        },
         floatingActionButton = {
             FloatingActionButton(
                 shape = CircleShape,
@@ -85,9 +91,20 @@ fun ListScreen(navController: NavController) {
                 }
             )
         },
-
+        topBar = {
+            AppTopBar(
+                navController = navController,
+                title = "List",
+                showBackIcon = false,
+                route = "form"
+            )
+        },
         content = {
-            //zawartość ekranu
+            LazyColumn(modifier = Modifier.padding(it)) {
+                items(items = todoTasks()) { item ->
+                    ListItem(item = item)
+                }
+            }
         }
 
     )
@@ -101,7 +118,7 @@ fun todoTasks(): List<TodoTask> {
     )
 }
 
-enum class Priority() {
+enum class Priority {
     High, Medium, Low
 }
 
@@ -116,17 +133,59 @@ fun ListItem(item: TodoTask, modifier: Modifier = Modifier) {
     ElevatedCard(
         modifier = modifier
             .fillMaxWidth()
-            .heightIn(120.dp)
             .padding(8.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 4.dp
         )
     ) {
-        //dodaj pozostałe funkcje tworzące komponenty z danymi elementu listy
-        Text(text = item.title)
-        //...
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = item.title,
+                    style = MaterialTheme.typography.titleLarge,
+                    overflow = TextOverflow.Ellipsis,
+                )
+                Text(
+                    text = "Deadline:\n" + item.deadline.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Priorytet:\n"+ item.priority,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+                Image(
+                    painter = painterResource(
+                        if (item.isDone) {
+                            R.drawable.completed
+                        } else {
+                            R.drawable.not_completed
+                        }
+                    ),
+                    contentDescription = "Status",
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.Bottom)
+                )
+
+            }
+        }
     }
 }
+
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun FormScreen(navController: NavController) {
@@ -140,7 +199,7 @@ fun FormScreen(navController: NavController) {
             )
         },
         content = {
-            Text("Formularz")
+            Text("Formularz TODO")
         }
     )
 }
@@ -178,7 +237,7 @@ fun AppTopBar(
             if (showBackIcon) {
                 IconButton(onClick = { navController.navigate(route) }) {
                     Icon(
-                        imageVector = Icons.Filled.ArrowBack,
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = "Back"
                     )
                 }
